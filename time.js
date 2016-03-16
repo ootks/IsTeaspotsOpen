54f69e4c0e08c7e47873fdd3ba268666e366eb23
Mark Gillespie <mark.gillespie81@verizon.net>
2016-03-16 02:54:53 -0800

    Replaced twitter link with the iframe directly

--- time.js
+++ time.js
@@ -211,9 +211,6 @@ function getBackground(){
 }
 
 $(document).ready(function(){
-    $(".social_media_buttons").hide();
-    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
-    $(".social_media_buttons").show();
     getBackground();
     displayInfo();
     setInterval(displayInfo, 1000);
